271dd3a759518942a451da1af1f988b1bd2269f4
AmanulFarhan <amanulfarhan2005@gmail.com>
2025-06-17 13:06:50 -0700

    fixed cloudinary bug

--- routes/blog.js
+++ routes/blog.js
@@ -2,19 +2,13 @@ const { Router } = require("express");
 const Blog = require("../models/blog");
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require("../cloudinary");
 const path = require("path");
 const Comment = require("../models/comment");
 
 const router = Router();
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const storage = new multer.CloudinaryStorage({
+const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
         folder: "blogify_uploads",
